931e73cc9060a6dbab5abce2f02aaea9511c45b2
Riyas Iqbal <riyasiqbal10@gmail.com>
2023-01-19 17:57:13 +0530

    first commit

---
+++ duplicateZero.js
@@ -0,0 +1,17 @@
+let arr = [2, 3, 4, 0, 8, 6]
+aiuhdai = [ 2, 3, 4, 0, 0, 8 ]
+
+var duplicateZeros = function (arr) {
+    const newArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        newArr.push(arr[i]);
+        arr[i] === 0 && newArr.push(0);
+    }
+    for (let i = 0; i < arr.length; i++) {
+        arr[i] = newArr[i];
+    }
+    console.log(arr)
+}
+
+
+duplicateZeros(arr)
---
+++ pairSum.js
@@ -0,0 +1,18 @@
+
+
+
+function hi(arr,a){
+    let count=0;
+    for(i=0;i<arr.length-1;i++){
+    for(j=i+1;j<arr.length;j++){
+        if(arr[i]+arr[j]==a){
+           count++;
+        }
+    }
+   }
+   console.log(count);
+}
+
+hi([2,4,6,8,0],8)
+
+
