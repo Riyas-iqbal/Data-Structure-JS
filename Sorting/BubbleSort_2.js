3fd25e1e2a51dd2ced8c82612e708e006dc8aeff
Riyas Iqbal <riyasiqbal10@gmail.com>
2023-02-08 20:01:26 +0530

    bubble sort alternate

---
+++ Sorting/BubbleSort_2.js
@@ -0,0 +1,19 @@
+// Bubble Sort
+function bubbleSort(arr) {
+    let n = arr.length;
+    for (let i = 0; i < n - 1; i++) {
+        for (let j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                let temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+    return arr;
+    }
+    
+    
+    
+    
+    console.log(bubbleSort([4,2,3,1]))
