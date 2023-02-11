724fbf53946b5dab5e0ab3e44f837e475b52b6a0
Riyas Iqbal <riyasiqbal10@gmail.com>
2023-02-11 19:04:58 +0530

    InsertionSort-2

--- Sorting/InsertionSort.js
+++ Sorting/InsertionSort.js
@@ -12,5 +12,16 @@ function InsertionSort(array) {
 
 const arr = [8, 20, -2, 4, -6]
 
+function InsertionSort2(arr) {
+    for (let i = 1; i < arr.length; i++) {
+        let currentElement = arr[i];
+        for (var j = i - 1; j >= 0 && arr[j] > currentElement; j--) {
+            arr[j+1] = arr[j];
+        }
+        arr[j+1] = currentElement;
+    }
+    return arr
+}
 
 
+console.log(InsertionSort2(arr));
