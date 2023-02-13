4a0a725bf7ca7f09b7ee2823a3fbca2fe4ffe892
Riyas Iqbal <riyasiqbal10@gmail.com>
2023-02-13 17:20:40 +0530

    helper method recursion

--- test.js
+++ test.js
@@ -42,13 +42,3 @@ function InsertionSort(arr){
     return arr
 }
 
-
-function factorial(num) {
-    if (num <= 1) {
-        return 1
-    }
-    return num * factorial(num-1)
-}
-
-
-console.log(factorial(4));
