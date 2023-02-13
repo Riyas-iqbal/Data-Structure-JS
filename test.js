e6e788bae4bab428dda611ef9c2ea39d4fc4137c
Riyas Iqbal <riyasiqbal10@gmail.com>
2023-02-13 14:18:08 +0530

    test1

--- test.js
+++ test.js
@@ -1,1 +1,24 @@
+function bubbleSort(arr) {
+    for (let i = 0; i < arr.length - 1; i++) {
+        let swapped = false
+        for (let j = 0; j < arr.length - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                [[arr[j]], [arr[j + 1]]] = [[arr[j + 1]], [arr[j]]]
+                swapped = true
+            }
+        }
+        if(!swapped) break;
+    }
+    return arr
+}
 
+let arr = [7,1,2,3,4,5,6]
+
+console.log(bubbleSort(arr))
+
+
+function SelectionSort(arr) {
+    for (let i = 0; i < arr.length - 1; i++) {
+        
+    }
+}
