52a8eb3593d5df480a138b40d573f6318aa4bb0d
Riyas Iqbal <riyasiqbal10@gmail.com>
2023-02-13 14:34:50 +0530

    test2

--- test.js
+++ test.js
@@ -7,18 +7,25 @@ function bubbleSort(arr) {
                 swapped = true
             }
         }
-        if(!swapped) break;
+        if (!swapped) break;
     }
     return arr
 }
 
-let arr = [7,1,2,3,4,5,6]
-
-console.log(bubbleSort(arr))
-
+let nearlySorted = [1, 2, 3, 5 , 4]
+let random = [4, 2, 5, 1, 3]
 
 function SelectionSort(arr) {
-    for (let i = 0; i < arr.length - 1; i++) {
-        
+    for (let i = 0; i < arr.length; i++) {
+        let minimum = i
+        for (let j = i+1; j < arr.length; j++) {
+            if (arr[j] < arr[minimum] ) {
+                minimum = j
+            }
+        }
+        if (i !== minimum) [[arr[i]],[arr[minimum]]] = [[arr[minimum]],[arr[i]]]
     }
+    return arr
 }
+
+console.log(SelectionSort(nearlySorted))
