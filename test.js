5c251fc63880b87d300fce271739d8067ed0e008
Riyas Iqbal <riyasiqbal10@gmail.com>
2023-02-22 23:30:28 +0530

    test3

--- test.js
+++ test.js
@@ -1,7 +1,8 @@
 class Node {
     constructor(data) {
         this.data = data
-        this.next = null
+        this.left = null
+        this.right = null
     }
 }
 
@@ -14,10 +15,38 @@ class BinarySearchTree {
         return this.root === null
     }
 
+    insert(value){
+        const node  = new Node(value)
+        if (this.isEmpty()) {
+             return this.root = node
+        }
+
+        this.insertNode(this.root,node)
+    }
+
+    insertNode(root,node){
+        if (root.data > node.data) {
+            if (!root.left) {
+                root.left = node
+            } else {
+                this.insertNode(root.left,node)
+            }
+        } else {
+            if (!root.right) {
+                root.right = node
+            } else {
+                this.insertNode(root.right,node)
+            }
+        }
+    }
     
 }
 
 
 const bst = new BinarySearchTree()
 
-console.log(bst.isEmpty())
+bst.insert(5)
+bst.insert(10)
+bst.insert(7)
+bst.insert(4)
+console.log(bst.root)
