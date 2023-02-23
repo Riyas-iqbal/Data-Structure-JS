6d7c6f929f6bc39bb6cd14014f5b4e06d81ef8a6
Riyas Iqbal <riyasiqbal10@gmail.com>
2023-02-23 22:30:12 +0530

    test3

--- test.js
+++ test.js
@@ -15,33 +15,47 @@ class BinarySearchTree {
         return this.root === null
     }
 
-    insert(value){
-        const node  = new Node(value)
+    insert(value) {
+        const node = new Node(value)
         if (this.isEmpty()) {
-             return this.root = node
+            return this.root = node
         }
 
-        this.insertNode(this.root,node)
+        this.insertNode(this.root, node)
     }
 
-    insertNode(root,node){
+    insertNode(root, node) {
         if (root.data > node.data) {
             if (!root.left) {
                 root.left = node
             } else {
-                this.insertNode(root.left,node)
+                this.insertNode(root.left, node)
             }
         } else {
             if (!root.right) {
                 root.right = node
             } else {
-                this.insertNode(root.right,node)
+                this.insertNode(root.right, node)
             }
         }
     }
-    
-}
 
+    isPresent(data, root = this.root) {
+        if (this.isEmpty()) {
+            return false
+        }
+
+        if (root.data == data) {
+            return true
+        } else if (root.data > data && root.left) {
+            this.isPresent(data, root.left)
+        } else if (root.data < data && root.right) {
+            this.isPresent(data, root.right)
+        } 
+        return -1
+    }
+
+}
 
 const bst = new BinarySearchTree()
 
@@ -50,3 +64,4 @@ bst.insert(10)
 bst.insert(7)
 bst.insert(4)
 console.log(bst.root)
+console.log(bst.isPresent(6))
