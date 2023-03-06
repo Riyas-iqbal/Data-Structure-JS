d3a23928140bf72261e0afc7d2145108011e2338
Riyas Iqbal <riyasiqbal10@gmail.com>
2023-03-06 19:36:30 +0530

    bracket match

--- test.js
+++ test.js
@@ -1,204 +1,59 @@
-class Node {
-    constructor(data) {
-        this.value = data
-        this.right = null
-        this.left = null
-    }
-}
+//   (() --->  1
+//   (()) --->  0
+//   ())( --->  0
 
-class QueueNode {
-    constructor(data) {
+
+class StackNode{
+    constructor(data){
         this.data = data
         this.next = null
     }
 }
 
-class Queue {
-    constructor() {
-        this.rear = null
-        this.front = null
+class Stack {
+    constructor(){
+        this.top = null
         this.size = 0
     }
 
-    enQueue(data) {
-        const node = new QueueNode(data)
+
+    push(data){
         this.size++
-        if (!this.front) {
-            this.rear = node
-            this.front = node
-            return this.rear
-        }
-        this.rear.next = node
-        this.rear = node
-        return this.rear
+        const node = new StackNode(data)
+        if (!this.top) return this.top = node
+        node.next = this.top
+        this.top = node
     }
 
-    deQueue() {
-        if (!this.front) {
-            return null
-        }
+    pop(){
         this.size--
-        let temp = this.front
-        this.front = this.front.next
-        return temp
-    }
-}
-
-class BinarySearchTree {
-
-    constructor() {
-        this.root = null
+        let lastElem =  this.top.data
+        this.top = this.top.next
+        return lastElem
     }
 
-    isEmpty() {
-        return this.root === null
+    peek(){
+        return this.top
     }
-
-    insert(value) {
-        const node = new Node(value)
-
-        if (this.isEmpty()) {
-            this.root = node
-            return this.root
-        }
-        //private method call
-        this.#insertNode(this.root, node)
-    }
-
-    //private methods introduced in ECMAscript 10 (2019)
-    #insertNode(root, node) {
-        if (root.value > node.value) {
-            if (!root.left) {
-                root.left = node
-            } else {
-                this.#insertNode(root.left, node)
-            }
-        } else {
-            if (!root.right) {
-                root.right = node
-            } else {
-                this.#insertNode(root.right, node)
-            }
-        }
-    }
-
-    search(value, root = this.root) {
-        if (!root) {
-            return false
-        }
-
-        if (root.value === value) {
-            return root
-        } else if (root.value > value) {
-            return this.search(value, root.left)
-        } else {
-            return this.search(value, root.right)
-        }
-    }
-
-    preOrder(root = this.root) {
-        if (root) {
-            console.log(root.value)
-            this.preOrder(root.left)
-            this.preOrder(root.right)
-        }
-    }
-
-    inOrder(root = this.root) {
-        if (root) {
-            this.inOrder(root.left)
-            console.log(root.value)
-            this.inOrder(root.right)
-        }
-    }
-
-    postOrder(root = this.root) {
-        if (root) {
-            this.postOrder(root.left)
-            this.postOrder(root.right)
-            console.log(root.value)
-        }
-    }
-
-    min(root = this.root) {
-        if (root.left) {
-            return this.min(root.left)
-        }
-        return root.value
-    }
-
-    max(root = this.root) {
-        if (root.right) {
-            return this.max(root.right)
-        }
-        return root.value
-    }
-
-    delete(value) {
-        if (this.isEmpty()) {
-            return null
-        }
-        this.root = this.deleteNode(value)
-        // console.log(this.root)
-    }
-
-    deleteNode(value, root = this.root) {
-        if (root.value > value) {
-            root.left = this.deleteNode(value, root.left)
-        } else if (root.value < value) {
-            root.right = this.deleteNode(value, root.right)
-        } else {
-            if (!root.right && !root.left) {
-                return null
-            } else if (!root.right) {
-                return root.left
-            } else if (!root.left) {
-                return root.right
-            } else {
-                root.value = this.min(root.right)
-                root.right = this.deleteNode(root.value, root.right)
-            }
-        }
-        return root
-    }
-
-    isValidBST(root = this.root) {
-
-        function isValidNode(node) {
-            if (!node) {
-                return true
-            }
-
-            if (node.right && node.right.value <= node.value) {
-                return false
-            }
-
-            if (node.left && node.left.value >= node.value) {
-                return false
-            }
-
-            return(isValidNode(node.left) && isValidNode(node.right))
-        }
-
-        return isValidNode(root)
-
-    }
-
+    
 }
 
 
-const bst = new BinarySearchTree()
-
-bst.insert(5)
-// bst.insert(4)
-// bst.insert(8)
-bst.insert(9)
-bst.insert(1)
-bst.insert(3)
-
-bst.delete(3)
-
-console.log(bst)
+const stack = new Stack()
 
 
-console.log(bst.isValidBST())
+function matchBracket(array){
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] == '(' || ')') {
+            if (stack.peek() == '(' && array[i] == ')' ) {
+                stack.pop()
+            } else {
+                stack.push(array[i])
+            }
+        }
+    }
+    return this.size
+}
+
+
+console.log(matchBracket([1,2,3,4,5,6]))
